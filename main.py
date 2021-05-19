9a7e98676877046b0d176424ca5185eab818c703
natalied <nataliedoyle@scopeeyecare.com>
2021-05-19 10:18:11 +0100

    Added CSV files

---
+++ main.py
@@ -0,0 +1,24 @@
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#project instructions
+# 1) Real World Scenario - Project should use a real world finance dataset and include a reference of their source in the report [1]
+# 2) Importing data - Retrieve data from online APIs [1] , Import a CSV file into a Pandas DataFrame. [1]
+# 3) Analyzing data - Your project should include sorting, indexing, grouping. [1]
+# Replace missing values or dropping duplicates. [1] , Looping, iterrows [1] , Merge dataframes [1]
+# 4) Python - Use functions to create reusable code. [1] , Numpy. [1] , Dictionary or Lists. [1]
+# 5) Visualize - Generate at least two charts using Matplotlib [2]
+# 6) Generate Valuable Insights - 5 insights from the visualization. [3]
+
+
+
+
+# 2 import a CSV file into Pandas DataFrame
+import pandas as pd
+#import matplotlib as plt
+Doge =pd.read_csv("Dogecoin Historical Data.csv")
+Bit =pd.read_csv("Bitcoin Historical Data.csv")
+print(Doge.info())
+print(Bit.info())
+
+print(Doge.head())
+print(Bit.head())
