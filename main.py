2519e2e4426d7cb474b825a9f7d638330b0ecb17
natalied <nataliedoyle@scopeeyecare.com>
2021-05-22 12:06:32 +0100

    Merge data sets

--- main.py
+++ main.py
@@ -10,21 +10,38 @@
 # 6) Generate Valuable Insights - 5 insights from the visualization. [3]
 
 
-
-
 # 2 import a CSV file into Pandas DataFrame
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 #import matplotlib as plt
 Doge =pd.read_csv("Dogecoin Historical Data.csv")
 Bit =pd.read_csv("Bitcoin Historical Data.csv")
-Google =pd.read_csv("GOOGL.csv",index_col="Date")
+Ggl =pd.read_csv("GOOGL.csv",index_col="Date")
 AMD = pd.read_csv("AMD.csv",index_col="Date")
 
-print(Google.head())
-print(AMD.head())
+#print(Ggl.head())
+#print(AMD.head())
 #print(Doge.head())
 #print(Bit.head())
 
-Google_vs_AMD=Google.merge(AMD,on="Date",how='left',suffixes=('_Google','_AMD'))
-print(Google_vs_AMD.head())
-print(Google_vs_AMD.info())
+Ggl_vs_AMD=Ggl.merge(AMD,on="Date",how='left',suffixes=('_Ggl','_AMD'))
+Ggl_AMD_Missing_values=Ggl_vs_AMD.isnull().sum()
+#print(Ggl_AMD_Missing_values)
+#print(Ggl_vs_AMD.head())
+#print(Ggl_vs_AMD.info())
+
+
+# Highest stock price.
+Highest = Ggl_vs_AMD.sort_values("High_Ggl",ascending=False)
+Highest_review=Highest[["High_Ggl","High_AMD"]]
+#print(Highest_review.head())
+
+Price_Compare=Ggl_vs_AMD[["Open_Ggl","Open_AMD"]]
+#print(Price_Compare.head())
+
+#ploting Google Vs AMD stock prices
+#Price_Compare.plot(x="Open_Ggl",kind="line")
+#plt.legend(["Google","AMD"])
+#plt.show()
